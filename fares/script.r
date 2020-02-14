c68d32ae09290336044d303fbda1dc9a402aa29e
Nate Wessel <bike756@gmail.com>
2020-02-14 11:29:20 -0500

    restructure

---
+++ fares/script.r
@@ -0,0 +1,15 @@
+library('tidyverse')
+d = read_csv('Dropbox/ttc-fares/TTC-annual-fares.csv')
+
+d %>% 
+  gather('Fare','price',-cpi,-year) %>%
+  group_by(Fare) %>%
+  mutate(
+    price19 = price * (max(cpi)/cpi),
+    price19s = scale(price19)
+  ) %>% 
+	ggplot( aes(x=year,y=price19s,color=Fare) ) + 
+		geom_line() + geom_point() +
+		labs(title='Relative Change in real TTC Fares 1973 - 2019',x='',y='Price (adjusted for inflation and standardized)') +
+		theme_minimal()
+
