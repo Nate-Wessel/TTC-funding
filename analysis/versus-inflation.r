35db966e4885eef9300e99427959feb39f1657a6
Nate Wessel <bike756@gmail.com>
2020-02-21 21:44:33 -0500

    add fare change graph

--- analysis/versus-inflation.r
+++ analysis/versus-inflation.r
@@ -15,6 +15,22 @@ fare = read_csv('TTC-funding/fares/nominal-fare-changes.csv') %>%
   )
 
 cpi %>% 
+  # join on Date
   left_join(fare) %>% 
-  ggplot( aes(x=Date,y=`Cash Fare`) ) + 
-  geom_line() + geom_point()
+  # fill in missing values
+  fill(`Cash Fare`,`Prepaid Fare`) %>%
+  rename(Cash=`Cash Fare`,`Ticket/Token`=`Prepaid Fare`) %>% 
+  gather(Cash,`Ticket/Token`,key='Fare Type',value='Fare') %>% 
+  filter(!is.na(Fare)) %>% 
+  group_by(`Fare Type`) %>% 
+  # standard CPI to the present
+  mutate(
+    CPI = CPI / max(CPI),
+    Inflated = Fare / CPI
+  ) %>%
+  ggplot( aes(x=Date) ) + 
+    geom_line(aes(y=Fare,color=`Fare Type`),alpha=0.75) + 
+    geom_line(aes(y=Inflated,color=`Fare Type`)) + 
+    scale_colour_manual(values=c('red3','darkcyan')) +
+    labs(title='TTC Fares - Nominal and Adjusted for Inflation') + 
+    theme_minimal()
