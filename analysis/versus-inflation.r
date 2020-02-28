7c1c68419fe07574a07be19f64938e5310f6b6c1
Nate Wessel <bike756@gmail.com>
2020-02-27 20:59:27 -0500

    added single zone fares prior to 1973

--- analysis/versus-inflation.r
+++ analysis/versus-inflation.r
@@ -4,14 +4,16 @@ library('lubridate')
 # read in the monthly CPI data 
 cpi = read_csv('TTC-funding/CPI/CanadaCPI.csv') %>%
   mutate(
-    Date = date( parse_date_time(Date,'ym') )
+    Date = date( parse_date_time(Date,'ym') ),
+    CPI = CPI / max(CPI)
   )
 
 # read in the fare increase dates
 fare = read_csv('TTC-funding/fares/nominal-fare-changes.csv') %>%
   mutate(
     # round dates to the nearest month, to match CPI
-    Date = round_date(Date,unit='month')
+    Date = round_date(Date,unit='month'),
+    
   )
 
 # recession data
@@ -27,25 +29,32 @@ cpi %>%
   # adjust passes
   mutate(
     `Monthly Pass` = `Monthly Pass` / (365/12*5/7*2),
-    `Day Pass` = `Day Pass` / 4
+    `Day Pass` = `Day Pass` / 4,
+    `Holiday Pass` = `Holiday Pass` / 4
   ) %>% 
   # fill in missing values
-  fill(`Cash`,`Ticket/Token`,`Monthly Pass`,`Day Pass`) %>%
-  gather(Cash,`Ticket/Token`,`Monthly Pass`,`Day Pass`,key='Fare Type',value='Fare') %>% 
-  filter( Fare > 0) %>% 
+  gather(
+    Cash, `Ticket/Token`,
+    `Monthly Pass`, `Day Pass`, `Holiday Pass`,
+    `1 Zone Ticket`, `1 Zone Cash`,
+    key='Fare Type',value='Fare'
+  ) %>% 
   group_by(`Fare Type`) %>% 
+  fill( Fare ) %>%
+  filter( Fare > 0) %>% 
   # standard CPI to the present
   mutate(
-    CPI = CPI / max(CPI),
     Inflated = Fare / CPI
   ) %>%
   ggplot() +
-    geom_line(aes(x=Date,y=Inflated,color=`Fare Type`)) + 
-    scale_colour_manual(values=c('red3','darkcyan','blue','orange')) +
-    labs(title='TTC Fares, Adjusted for Inflation') + 
-    theme_minimal() + 
     geom_rect(
       data=recessions,
       aes( xmin=start, xmax=end, ymin=0, ymax=Inf ),
       fill='pink',alpha=0.5
-    )
+    ) + 
+    geom_line(aes(x=Date,y=Inflated,color=`Fare Type`)) + 
+    scale_colour_manual(
+      values=c('red3','darkcyan','blue','orange','green','red','grey')
+    ) +
+    labs(title='TTC Fares, Adjusted for Inflation') + 
+    theme_minimal()
