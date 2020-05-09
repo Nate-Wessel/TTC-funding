c8d21f56459357ee0fa5210976a8e47647f31913
Nate Wessel <bike756@gmail.com>
2020-05-09 11:45:41 -0400

    plots for COVID analysis

--- analysis/COVID/plots.r
+++ analysis/COVID/plots.r
@@ -25,19 +25,28 @@ recessions = read_csv('TTC-funding/data/periodic/recessions.csv') %>%
 
 recession_bars = geom_rect(
 	data=recessions[-(1:2),],
-	aes( xmin=start, xmax=end, ymin=0, ymax=Inf ),
+	aes( xmin=start, xmax=end, ymin=-Inf, ymax=Inf, x=NULL, y=NULL ),
 	fill='pink', alpha=0.5
 )
 
 # yearly data
 rev_km = read_csv('TTC-funding/data/annual/revenue-km.csv') %>% 
-	mutate( Year = date( parse_date_time(Year,'y') ) )
+	mutate( 
+		Date = date( parse_date_time(Year,'y') ) + months(6)
+	)
+
+passengers = read_csv('TTC-funding/data/annual/revenue-passengers.csv') %>% 
+	mutate( 
+		Date = date( parse_date_time(Year,'y') ) + months(6) 
+	)
 
 funding = read_csv(
 		'TTC-funding/data/annual/funding.csv',
 		col_types = cols(.default = col_number())
 	) %>%
-	mutate( Year = date( parse_date_time(Year,'y') ) )
+	mutate( 
+		Date = date( parse_date_time(Year,'y') ) + months(6)
+	)
 
 
 key_years = date( parse_date_time(
@@ -57,19 +66,47 @@ my_x_axis = scale_x_date(
 		date_minor_breaks='5 years' 
 	)
 
+passengers %>%
+	ggplot( aes(x=Date,y=`revenue passengers`) ) + 
+		my_theme + recession_bars + my_x_axis +
+		labs(title='Revenue Passengers') + 
+		geom_line( color='gray40' ) +
+		geom_point( size=2.5,color='white',alpha=0.5 ) + 
+		geom_point( size=1 ) +
+		scale_y_continuous( 
+			labels=unit_format(unit="M",scale=1e-6),
+			limits=c(0,NA) 
+		) 
+
 rev_km %>%
 	mutate( metro = total - bus - streetcar ) %>%
-	gather( -Year, key='Mode', value='Revenue Miles' ) %>% 
-	ggplot() + my_theme + recession_bars +
-		geom_step( aes(x=Year,y=`Revenue Miles`,color=Mode) ) + 
-		scale_y_continuous( labels=unit_format(unit="M",scale=1e-6) ) +
-		my_x_axis + 
-		labs(title='Toronto Transit Commission - Revenue Kilometers')
+	gather( -Date, -Year, key='Mode', value='Revenue Miles' ) %>% 
+	ggplot( aes(x=Date,y=`Revenue Miles`) ) + 
+		my_theme + recession_bars + my_x_axis + 
+		labs(title='Revenue Kilometers') + 
+		geom_line( aes( color=Mode ) ) + 
+		geom_point( size=2,color='white',alpha=0.5 ) + 
+		geom_point( size=0.5 ) + 
+		scale_y_continuous( labels=unit_format(unit="M",scale=1e-6) ) 
 
-funding %>% 
-	
+# fares - nominal and inflated
+cpi %>% left_join(fare) %>%
+  select( Date, CPI, `1 Zone Ticket`,`1 Zone Cash`,`Cash`,`Ticket/Token` ) %>% 
+  gather( -Date, -CPI, key='Fare Type', value='Nominal Value' ) %>% 
+  group_by(`Fare Type`) %>% 
+  fill( `Nominal Value` ) %>% 
+	filter( `Nominal Value` > 0) %>% 
+  mutate( `Real 2020 Value` = `Nominal Value` / CPI ) %>%
+	gather( `Nominal Value`, `Real 2020 Value`, key='Value', value='Fare' ) %>% 
+  ggplot() + my_theme + recession_bars + my_x_axis + 
+		facet_grid(rows=vars(Value)) + 
+    geom_step(aes(x=Date,y=Fare,color=`Fare Type`)) + 
+    scale_colour_manual(
+      values=c('darkred','coral3','darkblue','darkcyan')
+    ) +
+    labs(title='Fares, Cash & Pre-paid') + 
+		scale_y_continuous( labels=dollar )
 
-	
 # operating funding STACKED
 funding %>% inner_join( cpi, by=c("Year"="Date") ) %>%
 	select( 
@@ -95,4 +132,4 @@ funding %>% inner_join( cpi, by=c("Year"="Date") ) %>%
       values=c('darkred','coral3','darkblue','darkcyan','blue')
     ) +
 		scale_y_continuous( labels=unit_format(unit="B",scale=1e-9) ) + 
-		labs(title='Toronto Transit Commission - Real 2020 Operations Funding')
+		labs(title='Operations Funding, Real 2020 Value')
